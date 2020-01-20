26bbd97a3c77b1238bafd537fdfe627b834b1192
Arif Rachmat Darmawan <rrdarmawan11@gmail.com>
2020-01-20 12:29:08 +0700

    Revisi Layout Login Dan Intent

--- app/src/main/java/com/example/wereview/ui/_login/login.java
+++ app/src/main/java/com/example/wereview/ui/_login/login.java
@@ -1,16 +1,48 @@
 package com.example.wereview.ui._login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wereview.MainActivity;
 import com.example.wereview.R;
+import com.example.wereview.ui._register.register;
 
 public class login extends AppCompatActivity {
+
+    Button btMasuk;
+    TextView textToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    btMasuk = findViewById(R.id.btMasuk);
+    textToRegister = findViewById(R.id.textToRegister2);
+
+    btMasuk.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    });
+
+    textToRegister.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), register.class);
+            startActivity(intent);
+            finish();
+        }
+    });
+
     }
 }
--- app/src/main/java/com/example/wereview/ui/_register/register.java
+++ app/src/main/java/com/example/wereview/ui/_register/register.java
@@ -1,14 +1,44 @@
 package com.example.wereview.ui._register;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.wereview.R;
+import com.example.wereview.ui._login.login;
 
 public class register extends AppCompatActivity {
+
+    private TextView tvMasuk;
+    private Button btDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvMasuk = findViewById(R.id.textToLogin2);
+        btDaftar = findViewById(R.id.btDaftar);
+
+        tvMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Nge Klik Coy!!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
