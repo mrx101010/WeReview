12936d1c051e479767a35b56d5015c9a9d193762
Arif Rachmat Darmawan <rrdarmawan11@gmail.com>
2020-01-27 11:19:29 +0700

    Membuat Layout Chat

---
+++ app/src/main/java/com/example/wereview/ui/_chatroom/ActivityChatroom.java
@@ -0,0 +1,16 @@
+package com.example.wereview.ui._chatroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wereview.R;
+
+public class ActivityChatroom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatroom);
+    }
+}
