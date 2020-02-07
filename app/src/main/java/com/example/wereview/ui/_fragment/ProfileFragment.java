36fa2d3eda9cef032587592ff2cc8486fcfb5a15
Arif Rachmat Darmawan <rrdarmawan11@gmail.com>
2020-02-07 13:50:07 +0700

    Manifest, Edit Profile

--- app/src/main/java/com/example/wereview/ui/_fragment/ProfileFragment.java
+++ app/src/main/java/com/example/wereview/ui/_fragment/ProfileFragment.java
@@ -1,15 +1,18 @@
 package com.example.wereview.ui._fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.wereview.R;
+import com.example.wereview.ui._edit_profil.EditActivity;
 
 public class ProfileFragment extends Fragment {
 
@@ -17,7 +20,10 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
     }
+
 }
 
 
